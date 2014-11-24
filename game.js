ee3d444e8c15b8b8ad4ec830598ab4dc4bdc5025
David Moreno Lumbreras <dmorenolumb@gmail.com>
2014-11-24 10:05:59 +0100

    Añadido prototipo de clase game.js con método iniciar partida

---
+++ game.js
@@ -0,0 +1,13 @@
+// Lós metodos van dentro de Meteor.methods ya que utilizamos Meteor para desarrollar el juego
+
+Meteor.methods({
+	//Prototipo de nueva partida
+	nuevaPartida: function(id_game){
+		Tablero = new Tablero();
+	};
+	
+
+	
+	
+});
+
