5448420ef8f37419954a139f027399e04ba08511
Alejandro Lorenzo Caro <a.lorenzoca@alumnos.urjc.es>
2014-11-25 18:51:01 +0100

    arreglo de aleatorio

--- spec/TableroSpec.js
+++ spec/TableroSpec.js
@@ -1,19 +1,19 @@
 describe("Pruebas relacionadas con las piezas",function(){
 	it("Podemos sacar piezas de forma aleatoria",function(){
-		piezas = ["Recto","Curva","Cruce4"];
-		npiezas = {"Recto": 1,"Curva":1,"Cruce4":1};
-		tablero = new Tablero(5,piezas,npiezas);
+		//piezas = ["Recto","Curva","Cruce4"];
+		//npiezas = {"Recto": 1,"Curva":1,"Cruce4":1};
+		tablero = new Tablero(5);
 
 		ran1 = tablero.saca_pieza();
 		ran2 = tablero.saca_pieza();
 		ran3 = tablero.saca_pieza();
 		
 		
-	//	expect(ran1).not.toBe(ran2);
-	//	expect(ran1).not.toBe(ran3);
-	//	expect(ran2).not.toBe(ran3);
+		expect(ran1).not.toBe(ran2);
+		expect(ran1).not.toBe(ran3);
+		expect(ran2).not.toBe(ran3);
 		
-		expect(tablero.piezas.totalPiezas).toBe(0);
+		expect(tablero.piezas.totalPiezas).toBe(67);
 		ran4 = tablero.saca_pieza();
 		expect(ran4).toBe(undefined);
 		
