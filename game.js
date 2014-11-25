7caa59647857179ef175294a49bd6b7daab44b17
David Moreno Lumbreras <dmorenolumb@gmail.com>
2014-11-25 11:52:35 +0100

    Añadido métodos de robar ficha y cambiar turno dentro del game.js

--- game.js
+++ game.js
@@ -7,9 +7,17 @@ Meteor.methods({
 		partida = new Juego(id_game, jugadores, 0);
 		//Ahora habría que iniciar el juego
 	};
+	//Al extraer la ficha el turno cambia automaticamente
+	robarFicha: function(id_game){
+		ficha = partida(id_game).tablero.saca_pieza();
+		return ficha;
+		//partida(id_game).tablero.azar
+	};
 	
-
 	
 	
+	cambiarTurno: function(){
+		return partida(id_game).listaJugadores[partida(id_game).tablero.azar];	
+	};
 });
 
