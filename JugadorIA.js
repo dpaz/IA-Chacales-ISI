bcf2ad6404a73ffad8fa9167a84d83b08fa633ed
jasaizc <ja.saizc@alumnos.urjc.es>
2014-12-03 12:49:24 +0100

    Añadida Version IA robar ficha y mirar si encaja

--- JugadorIA.js
+++ JugadorIA.js
@@ -1,6 +1,13 @@
 jugadorIA=function(id_jugador){
 
+    this.robar = function () {
+        var robado = Tablero.saca_pieza();
+        this.nuevaficha = new Pieza(robado, 0, 0);
+        var posicion = Tablero.posiblelugar(this.nuevaficha);
+        if (this.posicion.length == 0 && Tablero.totalFichas != 71) { this.robar() }
+    };
 
+    this.robar();
 
 
 
