77cbe2a3cf4b40b46f2aa6a04096fff2ea84a3a5
jasaizc <ja.saizc@alumnos.urjc.es>
2014-12-03 12:51:34 +0100

    Añadida version Con Tipos Jugada, y rotamos fichas

--- JugadorIA.js
+++ JugadorIA.js
@@ -9,14 +9,25 @@ jugadorIA=function(id_jugador){
 
     this.robar();
 
+    var TipoJugada = { puntos: 0, coorx: 0, coory: 0, giros: 0, }
+    var ProbarColocarFicha = function (ngiros) {
 
 
 
+    }
 
-
-
-
-
+    for (cont in this.nuevaficha.EncajaCon) {
+        ProbarColocarFicha(0);
+        this.nuevaficha = this.nuevaficha.girar();
+        ProbarColocarFicha(1);
+        this.nuevaficha = this.nuevaficha.girar();
+        this.nuevaficha = this.nuevaficha.girar();
+        colocarFicha(2);
+        this.nuevaficha = this.nuevaficha.girar();
+        this.nuevaficha = this.nuevaficha.girar();
+        this.nuevaficha = this.nuevaficha.girar();
+        colocarFicha(3);
+    }
 
     return [this.nuevaFicha.Tipo, TipoJugada];
 
