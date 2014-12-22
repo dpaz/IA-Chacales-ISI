6e37068348604e3c3e6b15ed6a6271eacb8eff34
David Moreno Lumbreras <dmorenolumb@gmail.com>
2014-12-22 18:30:11 +0100

    Avance cerrarCiudad, queda recursividad

--- cerrarCiudad.js
+++ cerrarCiudad.js
@@ -1,4 +1,5 @@
 cerrarCiudad = function(pieza,final){
+	var finalesCiudad = ["CiudadD", "CiudadE", "CiudadH", "CiudadI", "CiudadJ", "CiudadK", "CiudadL"];
 	var seguidoresEncontrados = [];
 	var puntos = 0;
 	var cerrado = 0;
@@ -38,5 +39,32 @@ cerrarCiudad = function(pieza,final){
 		if(ok[0] || ok[1] || ok[2] || ok[3]){return true;}
 	};
 	
-	
-	// FALTA IF DEL FINAL
+	if(finalesCiudad.idexOf(pieza.tipo)<0){
+	//esto significa que la pieza no es un final de ciudad por lo tanto tendrá que cerrarse por varios lados (minimo 2 lados)
+		cerrarCiudadRecur(pieza);
+		if(cerrado >= 2 || final){
+			this.sumar();
+			piezasRecorridas.forEach(function(pieza){
+				pieza.ciudadCerrada = true;
+			});
+			
+		}else{
+			puntuacion=0;
+			seguidoresEncontrados = [];
+		}
+	}else{
+	//es un final de ciudad por lo tanto tiene que haber como mínimo otro final de ciudad
+		cerrarCiudadRecur(pieza);
+		if(cerrado >= 1 || final){
+			this.sumar();
+			piezasRecorridas.forEach(function(pieza){
+				pieza.ciudadCerrada = true;
+			});
+			
+		}else{
+			puntuacion=0;
+			seguidoresEncontrados = [];
+		}
+	}
+	return [puntuacion, seguidoresEncontrados];
+}
