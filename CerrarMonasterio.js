55c64a8222bc9fd9d2190be67067ef25045d6da7
jasaizc <ja.saizc@alumnos.urjc.es>
2014-12-25 11:20:22 +0100

    Pruebas IA y cerrarMonasterio

--- CerrarMonasterio.js
+++ CerrarMonasterio.js
@@ -1,8 +1,22 @@
 
-CierraMonasterio = function(ficha,flag){
+CierraMonasterio = function (pieza, final) {
 		var puntos = 0;
-		var monje;
+		var seguidoresEncontrados = [];
+		var piezasTotales = 0;
 
+		this.sumar = function () {
+		    var sumados = [];
+		    seguidoresEncontrados.forEach(function (seguidor) {
+
+		        jugador = Tablero.listaSeguidores.find(function (jug) { jug == seguidor.jugador; });
+		        if (jugador.indexOf(sumados) >= 0) {
+		            jugador.puntuacion += puntos;
+		            sumados.push(jugador);
+		        }
+		        jugador.seguidores++;
+		    });
+
+		};
 
 
 
