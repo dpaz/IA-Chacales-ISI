6621b5593bb6e214c7fca5aa772f4bd0b0449dff
David Paz Brea <dpaz1993@gmail.com>
2014-12-27 12:22:25 +0100

    Modificado el metodo de retornar el valor en otroladron ya que producia bugs y nuevos test para comprobar su correcta funcionalidad

--- otroladron.js
+++ otroladron.js
@@ -2,7 +2,7 @@
 
 
 otroladron= function(pieza,posSeg,tablero,vengode){
-
+	var otro = false;
 
 	this.piezaArriba = function(pieza,posSeg,tablero,vengode){
 	
@@ -13,26 +13,26 @@ otroladron= function(pieza,posSeg,tablero,vengode){
 			
 			if(aux.tipo == 'Recto' || aux.tipo =='CiudadD'){
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguiguidores[i].tipo== 'ladron'){return true;}
+					if(aux.seguiguidores[i].tipo== 'ladron'){otro = true;return otro;}
 				}
 				//llamada recursiva para seguir comprobando el camino
 				return otroladron(aux,2,tablero,'Abajo');
 			}else if(aux.tipo == 'Curva' || aux.tipo=='CiudadJ' || aux.tipo=='CiudadK' || aux.tipo=='CiudadO'|| aux.tipo=='CiudadP'){
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguidores[i].tipo== 'ladron'){return true;}
+					if(aux.seguidores[i].tipo== 'ladron'){otro = true ;return otro;}
 				}
 				//En esta llamada recursiva compruebo antes el lado hacia donde esta el camino
 				if(aux.Derecha == 'Camino'){return otroladron(aux,6,tablero,'Abajo');}
 				else{return otroladron(aux,4,tablero,'Abajo');}
 			}else{
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguidores[i].tipo== 'ladron' && aux.seguidores[i].posicion == 8){return true}
+					if(aux.seguidores[i].tipo== 'ladron' && aux.seguidores[i].posicion == 8){otro = true;return otro}
 				}
 				//En caso de que sea uno de los cruces o el monasterio no hay recursividad ya
 			}
 		}
-
-		return false;
+		
+		return otro;
 	}
 
 	this.piezaAbajo = function(pieza,posSeg,tablero,vengode){
@@ -43,53 +43,55 @@ otroladron= function(pieza,posSeg,tablero,vengode){
 			//Primero compruebo que en esa pieza no hay ladrones
 			if(aux.tipo == 'Recto' || aux.tipo =='CiudadD'){
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguiguidores[i].tipo== 'ladron'){return true}
+					if(aux.seguiguidores[i].tipo== 'ladron'){otro= true;return otro}
 				}
 				//llamada recursiva para seguir comprobando el camino
 				return otroladron(aux,8,tablero,'Arriba');
 			}else if(aux.tipo == 'Curva' || aux.tipo=='CiudadJ' || aux.tipo=='CiudadK' || aux.tipo=='CiudadO'|| aux.tipo=='CiudadP'){
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguidores[i].tipo== 'ladron'){return true}
+					if(aux.seguidores[i].tipo== 'ladron'){otro = true;return otro}
 				}
 				//En esta llamada recursiva compruebo antes el lado hacia donde esta el camino
 				if(aux.Derecha == 'Camino'){return otroladron(aux,6,tablero,'Arriba');}
 				else{return otroladron(aux,4,tablero,'Arriba');}
 			}else{
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguidores[i].tipo== 'ladron' && aux.seguidores[i].posicion == 2){return true}
+					if(aux.seguidores[i].tipo== 'ladron' && aux.seguidores[i].posicion == 2){otro = true;return otro}
 				}
 				//En caso de que sea uno de los cruces o el monasterio no hay recursividad ya
 			}
 		}
-		return false;
+		
+		return otro;
 	}
 
 	this.piezaDerecha = function(pieza,posSeg,tablero,vengode){
-		//Pieza de la derecha
+		//Pieza de la derechaç
 		if(pieza.Derecha == 'Camino'&& tablero.piezaenposiciones(pieza.x+1,pieza.y) && tablero.piezaenposiciones(pieza.x+1,pieza.y).Izquierda=='Camino' && vengode !='Derecha'){
 			var aux =tablero.piezaenposiciones(pieza.x+1,pieza.y);
 			//Primero compruebo que en esa pieza no hay ladrones
 			if(aux.tipo == 'Recto' || aux.tipo =='CiudadD'){
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguidores[i].tipo== 'ladron'){return true}
+					if(aux.seguidores[i].tipo== 'ladron'){otro = true;return otro}
 				}
 				//llamada recursiva para seguir comprobando el camino
 				return otroladron(aux,6,tablero,'Izquierda');
 			}else if(aux.tipo == 'Curva' || aux.tipo=='CiudadJ' || aux.tipo=='CiudadK' || aux.tipo=='CiudadO'|| aux.tipo=='CiudadP'){
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguidores[i].tipo== 'ladron'){return true}
+					if(aux.seguidores[i].tipo== 'ladron'){otro = true;return otro}
 				}
 				//En esta llamada recursiva compruebo antes el lado hacia donde esta el camino
 				if(aux.Arriba == 'Camino'){return otroladron(aux,2,tablero,'Izquierda');}
 				else{return otroladron(aux,8,tablero,'Izquierda');}
 			}else{
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguidores[i].tipo== 'ladron' && aux.seguidores[i].posicion == 4){return true}
+					if(aux.seguidores[i].tipo== 'ladron' && aux.seguidores[i].posicion == 4){otro = true;return otro}
 				}
 				//En caso de que sea uno de los cruces o el monasterio o las entradas de ciudad no hay recursividad ya
 			}
 		}
-		return false;
+		
+		return otro;
 	}
 
 	this.piezaIzquierda = function(pieza,posSeg,tablero,vengode){
@@ -99,70 +101,72 @@ otroladron= function(pieza,posSeg,tablero,vengode){
 			//Primero compruebo que en esa pieza no hay ladrones
 			if(aux.tipo == 'Recto' || aux.tipo =='CiudadD'){
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguidores[i].tipo== 'ladron'){return true}
+					if(aux.seguidores[i].tipo== 'ladron'){otro = true;return otro}
 				}
 				//llamada recursiva para seguir comprobando el camino
 				return otroladron(aux,4,tablero,'Derecha');
 			}else if(aux.tipo == 'Curva' || aux.tipo=='CiudadJ' || aux.tipo=='CiudadK' || aux.tipo=='CiudadO'|| aux.tipo=='CiudadP'){
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguidores[i].tipo== 'ladron'){return true}
+					if(aux.seguidores[i].tipo== 'ladron'){otro = true;return otro}
 				}
 				//En esta llamada recursiva compruebo antes el lado hacia donde esta el camino
 				if(aux.Arriba == 'Camino'){return otroladron(aux,2,tablero,'Derecha');}
 				else{return otroladron(aux,8,tablero,'Derecha');}
 			}else{
 				for(i=0;i<aux.seguidores.length;i++){
-					if(aux.seguidores[i].tipo== 'ladron' && aux.seguidores[i].posicion == 6){return true}
+					if(aux.seguidores[i].tipo== 'ladron' && aux.seguidores[i].posicion == 6){otro = true;return otro}
 				}
 				//En caso de que sea uno de los cruces o el monasterio no hay recursividad ya
 			}
 		}
-		return false;
+
+		return otro;
 	}
+	
 	//Esto es para el caso de que nuestra pieza inicial tenga el seguidor
 	for(i=0;i<pieza.seguidores.length;i++){
-		if(pieza.seguidores[i].tipo="ladron"){return true}
+		if(pieza.seguidores[i].tipo="ladron"){otro = true; return otro}
 	}
 	if(pieza.tipo == 'Cruce4' || pieza.tipo== 'Cruce3' || pieza.tipo=='CiudadL'){
 		if(posSeg==2){
 			//Pieza de arriba
 
-			return piezaArriba(pieza,posSeg,tablero,vengode);
+			otro = piezaArriba(pieza,posSeg,tablero,vengode);
 
 		}
 
 		else if(posSeg==4){
 			//Pieza de la izquierda
-			return piezaIzquierda(pieza,posSeg,tablero,vengode);
+			otro = piezaIzquierda(pieza,posSeg,tablero,vengode);
 		}
 
 		else if(posSeg==6){
 			//Pieza de la derecha
-			return piezaDerecha(pieza,posSeg,tablero,vengode);
+			otro = piezaDerecha(pieza,posSeg,tablero,vengode);
 		}
 
 		else{
 			//Pieza de abajo
-			return piezaAbajo(pieza,posSeg,tablero,vengode);
+			otro = piezaAbajo(pieza,posSeg,tablero,vengode);
 		}
 	}
 	else{
 		//Pieza de arriba
-		return piezaArriba(pieza,posSeg,tablero,vengode);
+		otro = piezaArriba(pieza,posSeg,tablero,vengode);
 
 		//Pieza de abajo
-		return piezaAbajo(pieza,posSeg,tablero,vengode);
+		otro = piezaAbajo(pieza,posSeg,tablero,vengode);
 
 
 
 		//Pieza de la derecha
-		return piezaDerecha(pieza,posSeg,tablero,vengode);
+		otro = piezaDerecha(pieza,posSeg,tablero,vengode);
 
 
 		//Pieza de la izquierda
-		return piezaIzquierda(pieza,posSeg,tablero,vengode);
+		otro = piezaIzquierda(pieza,posSeg,tablero,vengode);
 	}
-	
+	return otro;
 }
 
 
--- spec/LadronesSpec.js
+++ spec/LadronesSpec.js
@@ -57,14 +57,14 @@ describe("Pruebas relacionadas con ladrones y caminos",function(){
 		
 		expect(otroladron(tablero.posiciones[0],8,tablero)).toBe(false);
 	});
-	/*
+	
 	it("Otro ladron comprueba correctamente por derecha si hay ladron",function(){
 		tablero = new Tablero(5)
 		pieza1 = new Pieza("Cruce4",0,0);
 		ladron = new Seguidor(4,"ladron",1);
-		pieza2 = new Pieza("Recto",0,1);
+		pieza2 = new Pieza("Recto",1,0);
 		pieza2 = pieza2.girar();
-		pieza3 = new Pieza("Curva",0,2);
+		pieza3 = new Pieza("Curva",2,0);
 		pieza3.seguidores.push(ladron);
 
 		tablero.posiciones.push(pieza1);
@@ -73,10 +73,92 @@ describe("Pruebas relacionadas con ladrones y caminos",function(){
 		
 		expect(otroladron(tablero.posiciones[0],6,tablero)).toBe(true);
 	});
+	
 	it("Otro ladron comprueba correctamente por derecha si no hay ladron",function(){
+		tablero = new Tablero(5)
+		pieza1 = new Pieza("Cruce4",0,0);
+		pieza2 = new Pieza("Recto",1,0);
+		pieza2 = pieza2.girar();
+		pieza3 = new Pieza("Curva",2,0);
 
+		tablero.posiciones.push(pieza1);
+		tablero.posiciones.push(pieza2);
+		tablero.posiciones.push(pieza3);
+		
+		expect(otroladron(tablero.posiciones[0],6,tablero)).toBe(false);
 	});
-	it("Otro ladron comprueba correctamente por izquierda",function(){
 
-	});*/
+	it("Otro ladron comprueba correctamente por izquierda si hay ladron",function(){
+	    tablero = new Tablero(5)
+		pieza1 = new Pieza("Cruce4",0,0);
+		ladron = new Seguidor(6,"ladron",1);
+		pieza2 = new Pieza("Recto",-1,0);
+		pieza2 = pieza2.girar();
+		pieza3 = new Pieza("Curva",-2,0);
+		pieza3 = pieza3.girar();
+		pieza3.seguidores.push(ladron);
+
+		tablero.posiciones.push(pieza1);
+		tablero.posiciones.push(pieza2);
+		tablero.posiciones.push(pieza3);
+		
+		expect(otroladron(tablero.posiciones[0],4,tablero)).toBe(true);
+	});
+	it("Otro ladron comprueba correctamente por izquierda si no hay ladron",function(){
+	    tablero = new Tablero(5)
+		pieza1 = new Pieza("Cruce4",0,0);
+		pieza2 = new Pieza("Recto",-1,0);
+		pieza2 = pieza2.girar();
+		pieza3 = new Pieza("Curva",-2,0);
+		pieza3 = pieza3.girar();
+		
+
+		tablero.posiciones.push(pieza1);
+		tablero.posiciones.push(pieza2);
+		tablero.posiciones.push(pieza3);
+		
+		expect(otroladron(tablero.posiciones[0],4,tablero)).toBe(false);
+	});
+
+	it("Otro ladron comprueba correctamente en un mapa complejo si hay ladron",function(){
+		tablero = new Tablero(5);
+		pieza1 = new Pieza("Recto",0,0);
+		pieza2 = new Pieza("Recto",0,1);
+		pieza3 = new Pieza("Curva",0,2);
+		pieza3 = pieza3.girar();
+		pieza4 = new Pieza("Curva",1,2);
+		pieza4 = pieza4.girar().girar().girar();
+		pieza5 = new Pieza("Cruce4",1,3);
+
+		ladron = new Seguidor(8,"ladron",1);
+		pieza5.seguidores.push(ladron);
+
+		tablero.posiciones.push(pieza1);
+		tablero.posiciones.push(pieza2);
+		tablero.posiciones.push(pieza3);
+		tablero.posiciones.push(pieza4);
+		tablero.posiciones.push(pieza5);
+
+		expect(otroladron(tablero.posiciones[0],2,tablero)).toBe(true);
+	});
+	it("Otro ladron comprueba correctamente en un mapa complejo si hay ladron",function(){
+		tablero = new Tablero(5);
+		pieza1 = new Pieza("Recto",0,0);
+		pieza2 = new Pieza("Recto",0,1);
+		pieza3 = new Pieza("Curva",0,2);
+		pieza3 = pieza3.girar();
+		pieza4 = new Pieza("Curva",1,2);
+		pieza4 = pieza4.girar().girar().girar();
+		pieza5 = new Pieza("Cruce4",1,3);
+
+		
+
+		tablero.posiciones.push(pieza1);
+		tablero.posiciones.push(pieza2);
+		tablero.posiciones.push(pieza3);
+		tablero.posiciones.push(pieza4);
+		tablero.posiciones.push(pieza5);
+
+		expect(otroladron(tablero.posiciones[0],2,tablero)).toBe(false);
+	});
 });
