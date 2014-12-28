c0dc9bd8c3315c899ef6c8703cf0763577e463cd
jasaizc <ja.saizc@alumnos.urjc.es>
2014-12-28 13:40:07 +0100

    Casi Terminado CierraMonasterio

--- CerrarMonasterio.js
+++ CerrarMonasterio.js
@@ -4,7 +4,7 @@ CierraMonasterio = function (pieza, final) {
 		var puntos = 0;
 		var monje;
 		var piezasTotales = 0;
-
+		var fichaAux;
 		this.sumar = function () {
 		    var sumados = [];
 		    seguidorEncontrado.forEach(function (seguidor) {
@@ -20,30 +20,77 @@ CierraMonasterio = function (pieza, final) {
 		var cerrarMon = function (ficha) {
 		    var TenemosMonje = false;
 		    var FichasCercanas = 0;
+		    var fichaAux;
 		    ficha.seguidores.forEach(function(seguidor){
 		        if(seguidor.tipo=='Monje'){
 		            monje = seguidor;
 		            TenemosMonje = true;
+		            FichasCercanas++;
 		        }  
 		    });
+		    if (TenemosMonje) {
+		        fichaAux = Tablero.piezaenposiciones(ficha.x + 1, ficha.y);
+		        if (fichaAux.lleno) { FichasCercanas++; }
+		        fichaAux = Tablero.piezaenposiciones(ficha.x - 1, ficha.y);
+		        if (fichaAux.lleno) { FichasCercanas++; }
+		        fichaAux = Tablero.piezaenposiciones(ficha.x, ficha.y + 1);
+		        if (fichaAux.lleno) { FichasCercanas++; }
+		        fichaAux = Tablero.piezaenposiciones(ficha.x, ficha.y - 1);
+		        if (fichaAux.lleno) { FichasCercanas++; }
+		        fichaAux = Tablero.piezaenposiciones(ficha.x + 1, ficha.y + 1);
+		        if (fichaAux.lleno) { FichasCercanas++; }
+		        fichaAux = Tablero.piezaenposiciones(ficha.x + 1, ficha.y - 1);
+		        if (fichaAux.lleno) { FichasCercanas++; }
+		        fichaAux = Tablero.piezaenposiciones(ficha.x - 1, ficha.y + 1);
+		        if (fichaAux.lleno) { FichasCercanas++; }
+		        fichaAux = Tablero.piezaenposiciones(ficha.x - 1, ficha.y - 1);
+		        if (fichaAux.lleno) { FichasCercanas++; }
+		        if (FichasCercanas == 9 || final == false)
+		        {
+		            puntos = FichasCercanas;
+		            sumarpuntos();
+		            console.log("Sumamos Por Cerrar Entero el Monasterio");
+		        }
+		        else if (final == true)
+		        {
+		            puntos = FichasCercanas;
+		            sumarpuntos();
+		            console.log("Sumamos Al ser Final de Partida");
+		        }
+		    }
+		};
 
-
-
-
-		    };
-
-
-		if ((ficha.tipo == "MonCamino") || (ficha.tipo == "MonGranja"))
-		{
-		    cerrarMon(ficha);
+		if (final == false) {
+		    if ((ficha.tipo == "MonCamino") || (ficha.tipo == "MonGranja"))
+		    { cerrarMon(ficha); }
+		    fichaAux = Tablero.piezaenposiciones(ficha.x + 1, ficha.y);
+		    if ((fichaAux.tipo == "MonCamino") || (fichaAux.tipo == "MonGranja"))
+		    { cerrarMon(fichaAux); }
+		    fichaAux = Tablero.piezaenposiciones(ficha.x - 1, ficha.y);
+		    if ((fichaAux.tipo == "MonCamino") || (fichaAux.tipo == "MonGranja"))
+		    { cerrarMon(fichaAux); }
+		    fichaAux = Tablero.piezaenposiciones(ficha.x, ficha.y + 1);
+		    if ((fichaAux.tipo == "MonCamino") || (fichaAux.tipo == "MonGranja"))
+		    { cerrarMon(fichaAux); }
+		    fichaAux = Tablero.piezaenposiciones(ficha.x, ficha.y - 1);
+		    if ((fichaAux.tipo == "MonCamino") || (fichaAux.tipo == "MonGranja"))
+		    { cerrarMon(fichaAux); }
+		    fichaAux = Tablero.piezaenposiciones(ficha.x + 1, ficha.y + 1);
+		    if ((fichaAux.tipo == "MonCamino") || (fichaAux.tipo == "MonGranja"))
+		    { cerrarMon(fichaAux); }
+		    fichaAux = Tablero.piezaenposiciones(ficha.x + 1, ficha.y - 1);
+		    if ((fichaAux.tipo == "MonCamino") || (fichaAux.tipo == "MonGranja"))
+		    { cerrarMon(fichaAux); }
+		    fichaAux = Tablero.piezaenposiciones(ficha.x - 1, ficha.y + 1);
+		    if ((fichaAux.tipo == "MonCamino") || (fichaAux.tipo == "MonGranja"))
+		    { cerrarMon(fichaAux); }
+		    fichaAux = Tablero.piezaenposiciones(ficha.x - 1, ficha.y - 1);
+		    if ((fichaAux.tipo == "MonCamino") || (fichaAux.tipo == "MonGranja"))
+		    { cerrarMon(fichaAux); }
+		}
+		else {
+		    if ((ficha.tipo == "MonCamino") || (ficha.tipo == "MonGranja"))
+		    { cerrarMon(ficha); }
 		}
 
-
-
-
-
-
-
-
-		return [puntuacion, seguidoresEncontrados];
 }
